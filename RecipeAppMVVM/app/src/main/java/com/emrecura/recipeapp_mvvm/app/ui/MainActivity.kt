e69405fdd9cc2066c062e14921144a056a1aa54f
Emre Cura <emre11cura@gmail.com>
2024-07-12 16:46:13 +0300

    Create Bottom Bar, Home and Search Fragment

--- RecipeAppMVVM/app/src/main/java/com/emrecura/recipeapp_mvvm/app/ui/MainActivity.kt
+++ RecipeAppMVVM/app/src/main/java/com/emrecura/recipeapp_mvvm/app/ui/MainActivity.kt
@@ -2,38 +2,62 @@ package com.emrecura.recipeapp_mvvm.app.ui
 
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.emrecura.recipeapp_mvvm.R
 import com.emrecura.recipeapp_mvvm.app.configs.ApiClient
 import com.emrecura.recipeapp_mvvm.app.data.Recipes
 import com.emrecura.recipeapp_mvvm.app.services.RecipeService
 import androidx.lifecycle.lifecycleScope
+import com.emrecura.recipeapp_mvvm.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var recipeService: RecipeService
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
+        binding.bottomNavigation.setOnItemSelectedListener {item ->
+            when(item.itemId) {
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.search -> replaceFragment(SearchFragment())
+            }
+            true
+
+        }
         recipeService = ApiClient.getClient().create(RecipeService::class.java)
 
-        lifecycleScope.launch {
-            getRecipes()
-        }
+        replaceFragment(HomeFragment())
 
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.frameLayout, fragment).commit()
+    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.home -> replaceFragment(HomeFragment())
+            R.id.search -> replaceFragment(SearchFragment())
+        }
+        return true
     }
 
      fun getRecipes() {
